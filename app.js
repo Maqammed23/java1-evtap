7bf219ae84237678baef89f3016ff09945204165
Maqammed23 <mehemmed23062012@gmail.com>
2025-09-26 18:04:25 +0400

    final

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-let city 
+let city = ("Quba");
+alert(city);
+const color = ("green");
+document.write(color);
+var potter = ("Harry Potter");
+console.log(potter)
+let rap = ("Rap")
+alert("Mənim sevdiyim musiqi janrı")
+alert(rap)
+let ad = ("Məhəmməd")
+let soyad = ("Rəhimov")
+document.write(ad)
+document.write(soyad)
